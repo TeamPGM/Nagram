e787b801c4e9fa5654ca0a5a719a8787ecc7707f
NekoInverter <chsqwyx@gmail.com>
2022-03-17 17:47:08 +0800

    Add option to show all rpc errors

--- TMessagesProj/src/main/kotlin/xyz/nextalone/nagram/NaConfig.kt
+++ TMessagesProj/src/main/kotlin/xyz/nextalone/nagram/NaConfig.kt
@@ -183,6 +183,11 @@ object NaConfig {
         ConfigItem.configTypeBool,
         false
     )
+    val showRPCError = addConfig(
+        "ShowRPCError",
+        ConfigItem.configTypeBool,
+        false
+    )
 
     fun addConfig(
         k: String,
