6a73e5c0c36e6e694755f35a05c6877ba8147b05
xtaodada <xtao@xtaolink.cn>
2022-05-09 20:54:32 +0800

    feat: stay in chats while banned

--- TMessagesProj/src/main/java/org/telegram/ui/ChatActivity.java
+++ TMessagesProj/src/main/java/org/telegram/ui/ChatActivity.java
@@ -19679,7 +19679,7 @@ public class ChatActivity extends BaseFragment implements NotificationCenter.Not
                 removeSelfFromStack();
                 fragment.finishFragment();
             } else {
-                finishFragment();
+                // finishFragment();
             }
         }
     }
