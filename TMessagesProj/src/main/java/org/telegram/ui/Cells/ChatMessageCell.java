1542cc8d81c5523ea40a5870e3d085a1da6e4cf5
xtaodada <xtao@xtaolink.cn>
2022-07-10 19:08:30 +0800

    fix: premium star color

--- TMessagesProj/src/main/java/org/telegram/ui/Cells/ChatMessageCell.java
+++ TMessagesProj/src/main/java/org/telegram/ui/Cells/ChatMessageCell.java
@@ -10948,7 +10948,7 @@ public class ChatMessageCell extends BaseCell implements SeekBar.SeekBarDelegate
                 if (currentUser.premium && NaConfig.INSTANCE.getShowPremiumStarInChat().Bool()) {
                     nameStringFinal = TextUtils.ellipsize(nameStringFinal, Theme.chat_namePaint, nameWidth - 2, TextUtils.TruncateAt.END);
                     SpannableStringBuilder spannableStringBuilder = SpannableStringBuilder.valueOf(String.format("  %s", nameStringFinal));
-                    spannableStringBuilder.setSpan(new ColoredImageSpan(ContextCompat.getDrawable(getContext(), R.drawable.msg_premium_liststar)), 0, 1, 0);
+                    spannableStringBuilder.setSpan(new ColoredImageSpan(ContextCompat.getDrawable(getContext(), R.drawable.msg_premium_liststar), Theme.getColor(Theme.key_chats_menuName)), 0, 1, 0);
                     nameStringFinal = spannableStringBuilder;
                 }
             }
@@ -11272,7 +11272,7 @@ public class ChatMessageCell extends BaseCell implements SeekBar.SeekBarDelegate
                         if (showReplyPremiumStar && NaConfig.INSTANCE.getShowPremiumStarInChat().Bool()) {
                             stringFinalName = TextUtils.ellipsize(stringFinalName, Theme.chat_namePaint, maxWidth - 2, TextUtils.TruncateAt.END);
                             SpannableStringBuilder spannableStringBuilder = SpannableStringBuilder.valueOf(String.format("  %s", stringFinalName));
-                            spannableStringBuilder.setSpan(new ColoredImageSpan(ContextCompat.getDrawable(getContext(), R.drawable.msg_premium_liststar)), 0, 1, 0);
+                            spannableStringBuilder.setSpan(new ColoredImageSpan(ContextCompat.getDrawable(getContext(), R.drawable.msg_premium_liststar), Theme.getColor(Theme.key_chats_menuName)), 0, 1, 0);
                             stringFinalName = spannableStringBuilder;
                         }
                         replyNameLayout = new StaticLayout(stringFinalName, Theme.chat_replyNamePaint, maxWidth + AndroidUtilities.dp(6), Layout.Alignment.ALIGN_NORMAL, 1.0f, 0.0f, false);
