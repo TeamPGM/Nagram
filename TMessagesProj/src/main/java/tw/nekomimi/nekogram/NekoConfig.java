ed6cdb874c225653256a9d36cbcdecfffa21c896
xtaodada <xtao@xtaolink.cn>
2022-07-29 22:10:53 +0800

    feat: Add datacenter status page
    Co-authored-by: NekoInverter <chsqwyx@gmail.com>

--- TMessagesProj/src/main/java/tw/nekomimi/nekogram/NekoConfig.java
+++ TMessagesProj/src/main/java/tw/nekomimi/nekogram/NekoConfig.java
@@ -30,6 +30,7 @@ public class NekoConfig {
 
     private static boolean configLoaded = false;
     private static final ArrayList<ConfigItem> configs = new ArrayList<>();
+    public static final ArrayList<DatacenterInfo> datacenterInfos = new ArrayList<>(5);
 
     // Configs
     public static ConfigItem migrate = addConfig("NekoConfigMigrate", configTypeBool, false);
@@ -247,6 +248,9 @@ public class NekoConfig {
                         }
                     }
                 }
+            }
+            for (int a = 1; a <= 5; a++) {
+                datacenterInfos.add(new DatacenterInfo(a));
             }
             configLoaded = true;
         }
@@ -466,6 +470,21 @@ public class NekoConfig {
             customAudioBitrate.setConfigInt(preferences.getInt("customAudioBitrate", 32));
         if (preferences.contains("disableGroupVoipAudioProcessing"))
             disableGroupVoipAudioProcessing.setConfigBool(preferences.getBoolean("disableGroupVoipAudioProcessing", false));
+    }
+
+    public static class DatacenterInfo {
+
+        public int id;
+
+        public long pingId;
+        public long ping;
+        public boolean checking;
+        public boolean available;
+        public long availableCheckTime;
+
+        public DatacenterInfo(int i) {
+            id = i;
+        }
     }
 
 }
