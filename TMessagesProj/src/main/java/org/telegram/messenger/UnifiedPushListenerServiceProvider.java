105444570f81ceb8a54cf036773fc17c52e9bcd3
xtaodada <xtao@xtaolink.cn>
2024-07-12 12:58:27 +0800

    ci: remove UnifiedPush

--- TMessagesProj/src/main/java/org/telegram/messenger/UnifiedPushListenerServiceProvider.java
+++ TMessagesProj/src/main/java/org/telegram/messenger/UnifiedPushListenerServiceProvider.java
@@ -3,7 +3,7 @@ package org.telegram.messenger;
 import android.os.SystemClock;
 import android.text.TextUtils;
 
-import org.unifiedpush.android.connector.UnifiedPush;
+//import org.unifiedpush.android.connector.UnifiedPush;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -13,7 +13,8 @@ public class UnifiedPushListenerServiceProvider implements PushListenerControlle
 
     @Override
     public boolean hasServices() {
-        return !UnifiedPush.getDistributors(ApplicationLoader.applicationContext, new ArrayList<>()).isEmpty();
+        return false;
+//        return !UnifiedPush.getDistributors(ApplicationLoader.applicationContext, new ArrayList<>()).isEmpty();
     }
 
     @Override
@@ -37,19 +38,19 @@ public class UnifiedPushListenerServiceProvider implements PushListenerControlle
             try {
                 SharedConfig.pushStringGetTimeStart = SystemClock.elapsedRealtime();
                 SharedConfig.saveConfig();
-                if (UnifiedPush.getAckDistributor(ApplicationLoader.applicationContext) == null) {
-                    List<String> distributors = UnifiedPush.getDistributors(ApplicationLoader.applicationContext, new ArrayList<>());
-                    if (distributors.size() > 0) {
-                        String distributor =  distributors.get(0);
-                        UnifiedPush.saveDistributor(ApplicationLoader.applicationContext, distributor);
-                    }
-                }
-                UnifiedPush.registerApp(
-                        ApplicationLoader.applicationContext,
-                        "default",
-                        new ArrayList<>(),
-                        "Telegram Simple Push"
-                );
+//                if (UnifiedPush.getAckDistributor(ApplicationLoader.applicationContext) == null) {
+//                    List<String> distributors = UnifiedPush.getDistributors(ApplicationLoader.applicationContext, new ArrayList<>());
+//                    if (distributors.size() > 0) {
+//                        String distributor =  distributors.get(0);
+//                        UnifiedPush.saveDistributor(ApplicationLoader.applicationContext, distributor);
+//                    }
+//                }
+//                UnifiedPush.registerApp(
+//                        ApplicationLoader.applicationContext,
+//                        "default",
+//                        new ArrayList<>(),
+//                        "Telegram Simple Push"
+//                );
             } catch (Throwable e) {
                 FileLog.e(e);
             }
